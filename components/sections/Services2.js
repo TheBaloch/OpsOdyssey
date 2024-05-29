4b21ea9596ad9019c2b8d28dbba36ed941f8be24
MissLahore <misslailariaz@gmail.com>
2024-05-29 19:59:44 +0500

    why we better

--- components/sections/Services2.js
+++ components/sections/Services2.js
@@ -9,7 +9,7 @@ export default function Services2() {
                         <div className="col-lg-6">
                             <div className="section-title white-title mb-50 tg-heading-subheading animation-style3">
                                 <span className="sub-title">Why We Are The Best</span>
-                                <h2 className="title tg-element-title">Let’s Discover Our Service Features Charter</h2>
+                                <h2 className="title tg-element-title">Unveil the Magic of Our Service Features Charter</h2>
                             </div>
                         </div>
                     </div>
@@ -17,11 +17,10 @@ export default function Services2() {
                         <div className="col-xl-3 col-lg-4 col-md-6">
                             <div className="services__item-two">
                                 <div className="services__icon-two">
-                                    <i className="flaticon-pie-chart" />
-                                </div>
+                                <img src="/assets/img/icon/collaborate.png" />                                </div>
                                 <div className="services__content-two">
-                                    <h2 className="title"><Link href="/services-details">Business Analytics</Link></h2>
-                                    <p>Morem area are psum dolor sitteme odern sectet aweur adipiscing always.</p>
+                                    <h2 className="title"><Link href="/services-details">Integrated Solutions</Link></h2>
+                                    <p>Emphasize the ability to provide end-to-end solutions, from telephony systems to cloud infrastructure and DevOps practices.</p>
                                     <Link href="/services-details" className="btn">Read More</Link>
                                 </div>
                             </div>
@@ -29,11 +28,11 @@ export default function Services2() {
                         <div className="col-xl-3 col-lg-4 col-md-6">
                             <div className="services__item-two">
                                 <div className="services__icon-two">
-                                    <i className="flaticon-budget" />
+                                <img src="/assets/img/icon/team.png" />
                                 </div>
                                 <div className="services__content-two">
-                                    <h2 className="title"><Link href="/services-details">Investment</Link></h2>
-                                    <p>Morem area are psum dolor sitteme odern sectet aweur adipiscing always.</p>
+                                    <h2 className="title"><Link href="/services-details">Expert Team</Link></h2>
+                                    <p>Expert Team Driving Growth with Advanced Web Solutions,Data Insights, and Secure Platforms for 100% Client Satisfaction.</p>
                                     <Link href="/services-details" className="btn">Read More</Link>
                                 </div>
                             </div>
@@ -41,11 +40,11 @@ export default function Services2() {
                         <div className="col-xl-3 col-lg-4 col-md-6">
                             <div className="services__item-two">
                                 <div className="services__icon-two">
-                                    <i className="flaticon-piggy-bank" />
+                                <img src="/assets/img/icon/reputation.png" />
                                 </div>
                                 <div className="services__content-two">
-                                    <h2 className="title"><Link href="/services-details">Tax Advisory</Link></h2>
-                                    <p>Morem area are psum dolor sitteme odern sectet aweur adipiscing always.</p>
+                                    <h2 className="title"><Link href="/services-details">Customer Focus</Link></h2>
+                                    <p>Discuss the company's commitment to understanding client needs and delivering customized solutions</p>
                                     <Link href="/services-details" className="btn">Read More</Link>
                                 </div>
                             </div>
@@ -53,11 +52,11 @@ export default function Services2() {
                         <div className="col-xl-3 col-lg-4 col-md-6">
                             <div className="services__item-two">
                                 <div className="services__icon-two">
-                                    <i className="flaticon-target" />
+                                <img src="/assets/img/icon/efficiency.png" />
                                 </div>
                                 <div className="services__content-two">
-                                    <h2 className="title"><Link href="/services-details">Target Market</Link></h2>
-                                    <p>Morem area are psum dolor sitteme odern sectet aweur adipiscing always.</p>
+                                    <h2 className="title"><Link href="/services-details">Performance Optimization</Link></h2>
+                                    <p>Deliver fast-loading, responsive websites with optimized code and efficient resource utilization.</p>
                                     <Link href="/services-details" className="btn">Read More</Link>
                                 </div>
                             </div>

091c2c6f19fa4f9e4285c02f3bbe41af35405909
MissLahore <misslailariaz@gmail.com>
2024-09-02 18:44:41 +0500

    ui/ux page done

--- app/latest-ui-ux/page.js
+++ app/latest-ui-ux/page.js
@@ -1,5 +1,6 @@
 import Faq from "@/components/Faq/Faq"
 import Faq2 from "@/components/Faq2/Faq2"
+import Faq3 from "@/components/Faq3/Faq3"
 import Layout from "@/components/layout/Layout"
 import BrandActiveSlider from "@/components/slider/BrandActiveSlider"
 import Tech from "@/components/Tech/Tech"
@@ -18,7 +19,7 @@ export default function Page() {
                                     <h2 className="title">Latest UI/UX</h2>
                                 </div>
                                 <div>
-                                <p className="text-lg leading-relaxed indent-8 mb-6">Give your business an online makeover with the help of our experienced SEO professionals, innovative strategists, and data-driven analysts. Our custom SEO solution works hand in hand with the objective of making your site rank among the first of the search engines. Our approach includes optimization of your site's content, technical structure, and backlink profile to drive targeted organic traffic to your website. We do not only keep up with the evolution of SEO trends but adjust our strategies proactively to suit your own needs and to make sure your search engine performance gets measurably better. The idea is to assist you even better in increasing rankings, attracting better-qualified visitors, and, eventually, encouraging sustainable growth in today's fiercely competitive digital environment.</p>                          
+                                <p className="text-lg leading-relaxed indent-8 mb-6">Turn your digital vision into an unforgettable experience with our UI/UX design services. Backed by an ensemble of visionary designers and user experience maestros, we merge creativity with functionality to design interfaces that intrigue and involve. We dive deep to understand your audience, blending intuitive design with bleeding edge aesthetics to create seamless and delightful interactions. The process starts from idea creation and wireframe development, goes on to professional design creation, and includes usability tests. We turn your ideas into a dynamic, user-friendly masterpiece. We don't design; we craft digital experiences that have an impact, please, and catalyze beneficial interactions between your brand and its users.</p>                          
                                 </div>
                                 
                                  
@@ -26,7 +27,7 @@ export default function Page() {
                         </div>
                         <div className="col-lg-3 col-md-9 col-sm-10 text-end mb-35">
                             <div className="about__img-wrap3-home8">
-                                <img src="assets/img/icon/seopage.png" alt="" />
+                                <img src="assets/img/icon/uiuxpage.png" alt="" />
                             </div>
                         </div>
                        </div>
@@ -48,14 +49,14 @@ export default function Page() {
                         <div className="col-lg-10 mb-35">
                             <div className="about__content-four ">
                             <div className="section-title mb-25">
-                                    <h2 className="title wow ">Why Choose OpsOdyssey for SEO?</h2>
+                                    <h2 className="title wow ">Why Choose OpsOdyssey for UI/UX?</h2>
                                 </div>
                                
                                     <div className="about__list-box " >
                                         <ul className="list-wrap mb-4">
-                                            <li><i className="flaticon-arrow-button" />We use advanced techniques and experience for effective enhancement in the search engine ranking of your website.</li>
-                                            <li><i className="flaticon-arrow-button" />Provide absolutely timely notifications of change so that our SEO efforts are perfectly aligned with your business goals.</li>
-                                            <li><i className="flaticon-arrow-button" />Fully optimize each component of your SEO strategy to drive improved visibility and demonstrable, quantifiable outcomes.</li>
+                                            <li><i className="flaticon-arrow-button" />We  leverage the latest trends and technologies in the development of catchy interfaces-Visually appealing and workable. </li>
+                                            <li><i className="flaticon-arrow-button" /> We design user interface to reach your target audience's heart for intuitive engaging experiences.</li>
+                                            <li><i className="flaticon-arrow-button" />From developing concepts to final implementation, we do full-cycle UI/UX design to round out a seamless user experience.</li>
                                             
                                         </ul>
                                     </div>
@@ -64,7 +65,7 @@ export default function Page() {
                         </div>
                         <div className="col-lg-2 col-md-9 col-sm-10 text-end mb-35">
                             <div className="about__img-wrap3-home8">
-                                <img src="assets/img/icon/seopage2.png" alt="" />
+                                <img src="assets/img/icon/uiuxpage2.png" alt="" />
                                 
                             </div>
                         </div>
@@ -85,50 +86,51 @@ export default function Page() {
                                                 <div className="row">
                                                 <div className="section-title text-center mb-40 ">
                                                        <span className="sub-title">WHAT WE OFFER</span>
-                                                       <h2 className="title">SEO Services Charter</h2>
+                                                       <h2 className="title">UI/UX Services Charter</h2>
                                                 </div>
                                                     <div className="col-md-6">
                                                         <div className="services__details-list-box">
                                                             <div className="icon">
-                                                                <img src="assets/img/icon/techseo.png" />
+                                                                <img src="assets/img/icon/wireframe.png" />
                                                             </div>
                                                             <div className="content">
-                                                                <h4 className="title">Technical SEO</h4>
-                                                                <p>Our team takes care of all the technical elements on your site, with a focus on site speed, friendliness on mobile devices, and crawlability to ensure your website is fully optimized.</p>
+                                                                <h4 className="title">Wireframing and Prototyping</h4>
+                                                                <p>We are good at wireframing and prototyping-creating interactive models of your product that show structure and flow. Prototyping allows for the proper visualization of ideas, testing functionality, and even gathering feedback before actual development kicks in, saving time and resources for our clients.</p>
                                                             </div>
                                                         </div>
                                                     </div>
                                                     <div className="col-md-6">
                                                         <div className="services__details-list-box">
                                                             <div className="icon">
-                                                              <img src="assets/img/icon/keyseo.png" />
+                                                            <img src="assets/img/icon/search-user.png" />
                                                             </div>
                                                             <div className="content">
-                                                                <h4 className="title">Keyword Research</h4>
-                                                                <p>We do extensive research to find all the most relevant keywords and phrases your target audience is using to find your services or products, thereby driving better-qualified traffic to your site.</p>
+                                                                <h4 className="title">User Research</h4>
+                                                                <p>User Research is a specialist area of OpsOdyssey, whereby in-depth insight will be gained into audiences in order to build solutions that make a dent. The specialized research methodologies align your product to meet the needs of the user and enhance its functionality and engagement.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="services__details-list-box">
+                                                            <div className="icon">
+                                                              <img src="assets/img/icon/uidesign.png" />
+                                                            </div>
+                                                            <div className="content">
+                                                                <h4 className="title">UI Design</h4>
+                                                                <p>Our UI Design services are tailored to the development of visually appealing, intuitive interfaces that raise user interaction. Be sure your product isn't only esthetically pleasing but will also be easy to navigate for seamless user experiences.</p>
                                                             </div>
                                                         </div>
                                                     </div>
                                                    
+                                                   
                                                     <div className="col-md-6">
                                                         <div className="services__details-list-box">
                                                             <div className="icon">
-                                                            <img src="assets/img/icon/linkseo.png" />
+                                                               <img src="assets/img/icon/person.png" />
                                                             </div>
                                                             <div className="content">
-                                                                <h4 className="title">Link Building</h4>
-                                                                <p>We design and implement strategies to achieve high-quality backlinks from authoritative sources that boost your website's authority and its search engine ranking positions.</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-md-6">
-                                                        <div className="services__details-list-box">
-                                                            <div className="icon">
-                                                               <img src="assets/img/icon/audit.png" />
-                                                            </div>
-                                                            <div className="content">
-                                                                <h4 className="title">SEO Audits</h4>
-                                                                <p>Full-scale SEO audits will look at your website's current performance, identify areas for improvement, and provide actionable recommendations that will help improve your SEO efforts.</p>
+                                                                <h4 className="title">Usability Testing</h4>
+                                                                <p>Our Usability Testing service will make sure real users identify points of friction in order to provide actionable insights that guarantee a user-friendly and functional end product, ready to deliver a seamless and engaging experience.</p>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -137,7 +139,7 @@ export default function Page() {
                                             </section>
                                             {/* faq start */}
                                         </div>
-                                        <Faq2/>
+                                        <Faq3/>
                                     </div>
                                 </div>
                             </div>
---
+++ components/Faq3/Faq3.js
@@ -0,0 +1,90 @@
+
+'use client'
+import { useState } from 'react'
+
+
+export default function Faq3() {
+    const [isActive, setIsActive] = useState({
+        status: false,
+        key: "",
+    })
+
+    const handleClick = (key) => {
+        if (isActive.key === key) {
+            setIsActive({
+                status: false,
+            })
+        } else {
+            setIsActive({
+                status: true,
+                key,
+            })
+        }
+    }
+    return (
+        <>
+            <section className="faqs__area-six pb-4 pt-5">
+                <div className="circle" data-parallax="{&quot;x&quot; : 100 , &quot;y&quot; : 100 }" />
+                <div className="container">
+                    <div className="row align-items-center">         
+                            <div className="box-faq-right">
+                            <div className="section-title text-center mb-40">
+                                     <span className="sub-title">FAQS</span>
+                                      <h2 className="title">Frequently asked questions</h2>
+                             </div>
+                                {/* <p className="tg-element-title mb-40">Our power of choice is untrammelled and when nothing preventsbeing able to do what we like best every pleasure.</p> */}
+                                <div className="block-faqs">
+                                    <div className="accordion wow fadeInUp" id="accordionFAQ">
+                                        <div className="accordion-item">
+                                            <h5 className="accordion-header" onClick={() => handleClick(1)}>
+                                                <button className={isActive.key == 1 ? "accordion-button text-heading-5" : "accordion-button text-heading-5 collapsed"} >
+                                                What is the difference between UI and UX design?
+                                                </button>
+                                            </h5>
+                                            <div className={isActive.key == 1 ? "accordion-collapse collapse show" : "accordion-collapse collapse"}>
+                                                <div className="accordion-body">UI design takes care of all the visuals while UX handles the whole experience and ease of use. Both are important to make a product successful.</div>
+                                            </div>
+                                        </div>
+                                        <div className="accordion-item">
+                                            <h5 className="accordion-header" onClick={() => handleClick(3)}>
+                                                <button className={isActive.key == 3 ? "accordion-button text-heading-5" : "accordion-button text-heading-5 collapsed"} >
+                                                How does good UI design affect my product?
+                                                </button>
+                                            </h5>
+                                            <div className={isActive.key == 3 ? "accordion-collapse collapse show" : "accordion-collapse collapse"}>
+                                                <div className="accordion-body">With good UI design, the product becomes more intuitive and user-engaging; it offers a good first impression to your users.</div>
+                                            </div>
+                                        </div>
+                                        <div className="accordion-item">
+                                            <h5 className="accordion-header" onClick={() => handleClick(4)}>
+                                                <button className={isActive.key == 4 ? "accordion-button text-heading-5" : "accordion-button text-heading-5 collapsed"} >
+                                                Why do I need wireframes and prototypes?
+                                                </button>
+                                            </h5>
+                                            <div className={isActive.key == 4 ? "accordion-collapse collapse show" : "accordion-collapse collapse"}>
+                                                <div className="accordion-body"> They let you visualize and test the layout and functionality of your product upfront, which saves time and money by working out problems beforehand.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="accordion-item">
+                                            <h5 className="accordion-header" onClick={() => handleClick(2)}>
+                                                <button className={isActive.key == 2 ? "accordion-button text-heading-5" : "accordion-button text-heading-5 collapsed"} >
+                                                Why is user research so important for my project?
+                                                </button>
+                                            </h5>
+                                            <div className={isActive.key == 2 ? "accordion-collapse collapse show" : "accordion-collapse collapse"}>
+                                                <div className="accordion-body">It helps make sure that your product will meet the real needs of your users by understanding their behaviors, pain points, and preferences.</div>
+                                            </div>
+                                        </div>
+                                       
+                                    </div>
+                                </div>
+                            </div>
+                        
+                    </div>
+                </div>
+            </section>
+
+        </ >
+    )
+}
